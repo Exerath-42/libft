ae1ff65b4371159a7c3765aea6d77a45f3953d0c
Exerath-42 <82114066+Exerath-42@users.noreply.github.com>
2022-01-07 06:44:26 +0300

    Add files via upload

---
+++ src/ft_strnstr.c
@@ -0,0 +1,18 @@
+#include "../includes/libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	nlen;
+
+	if (!*needle)
+		return ((char *)haystack);
+	nlen = ft_strlen(needle);
+	while (*haystack != '\0' && nlen <= len--)
+	{
+		if (ft_memcmp(haystack, needle, nlen) == 0)
+			return ((char *)haystack);
+		else
+			haystack++;
+	}
+	return (NULL);
+}
---
+++ src/ft_strtrim.c
@@ -0,0 +1,44 @@
+#include "../includes/libft.h"
+
+static int	isinset(char c, char const *set)
+{
+	int	i;
+
+	i = 0;
+	while (set[i] !='\0')
+	{
+		if (c == set[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		i;
+	int		start;
+	int		end;
+	char	*trimmed;
+
+	if (!s1)
+		return (NULL);
+	i = 0;
+	start = 0;
+	while (s1[start] && isinset(s1[start], set))
+		start++;
+	end = ft_strlen(s1);
+	while (end > start && isinset(s1[end - 1], set))
+		end--;
+	trimmed = malloc(sizeof(char) * (end - start));
+	if (!trimmed)
+		return (NULL);
+	while (start != end)
+	{
+		trimmed[i] = s1[start];
+		i++;
+		start++;
+	}
+	trimmed[i] = '\0';
+	return (trimmed);
+}
